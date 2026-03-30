1c02049662b262082685c15d82999a03da951451
RamiroHH <ramiroh.dev@gmail.com>
2026-03-29 22:20:41 -0300

    Update IProductoRepository.java

--- src/main/java/com/programacion4/unidad3ej3/feature/producto/repositories/IProductoRepository.java
+++ src/main/java/com/programacion4/unidad3ej3/feature/producto/repositories/IProductoRepository.java
@@ -1,12 +1,12 @@
 package com.programacion4.unidad3ej3.feature.producto.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.programacion4.unidad3ej3.feature.producto.models.Producto;
 
 @Repository
-public interface IProductoRepository extends CrudRepository<Producto, Long> {
+public interface IProductoRepository extends JpaRepository<Producto, Long> {
 
     boolean existsByNombre(String nombre);
     
